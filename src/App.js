9556fb7328873024f238914ec8cd592212609014
alex-glomo <alexandr@glomo.se>
2020-01-31 15:23:41 +0100

    Initial commit with the app generated via CRA and AnimatedAmount component.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
 import './App.css';
+import AnimatedAmount from './components/AnimatedAmount';
 
 function App() {
+  const [start, toggle] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <AnimatedAmount number={'0987654321'} start={start}/>
+      <AnimatedAmount number={2050} start={start}/>
+      <AnimatedAmount number={2020} start={start}/>
+      <AnimatedAmount number={2030} start={start}/>
+      <AnimatedAmount number={2019} start={start}/>
+      <AnimatedAmount number={2018} start={start}/>
+      <AnimatedAmount number={2016} start={start}/>
+      <AnimatedAmount number={2014} start={start}/>
+      <AnimatedAmount number={2011} start={start}/>
+      <AnimatedAmount number={2007} start={start}/>
+      <AnimatedAmount number={1990} start={start}/>
+      <AnimatedAmount number={1234567890} start={start}/>
+      <div className={'Controls'}>
+        <button onClick={() => toggle(false)}>Reset</button>
+        <button onClick={() => toggle(true)}>Start</button>
+      </div>
+    </>
   );
 }
 
